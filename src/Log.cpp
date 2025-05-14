5113ea5138982734ccbb4bbaa56d6573fcf971b5
gyh <3230194834@qq.com>
2025-05-14 09:54:58 -0700

    modify log.h

--- src/Log.cpp
+++ src/Log.cpp
@@ -3,7 +3,7 @@
 #include <utility>
 
 using LOGPAIR=std::pair<std::string, int>;
-std::unordered_map<std::string, int> Log::valMap={LOGPAIR("DEBUG",3),LOGPAIR("INFO",2),LOGPAIR("FATAL",1)};
+std::unordered_map<std::string, int> Log::valMap={LOGPAIR("DEBUG",3),LOGPAIR("INFO",2),LOGPAIR("ERROR",1),LOGPAIR("FATAL",0)};
 
 void Log::LOG_PRINT(const std::string& level,const std::string msg,const char* filename,int line){
     int curSetVal=valMap[CUR_LEVEL];
